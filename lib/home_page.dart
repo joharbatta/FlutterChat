4528be76a6f6689da31c822453e9a160ddf613d3
joharbatta <joharbatta@gmail.com>
2019-09-01 16:38:25 +0530

    Chat App using Flutter

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+class HomePage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+
+        title: new Text("Flutter Chat"),
+
+      ),
+      body: new ChatScreen()
+    );
+  }
+}
